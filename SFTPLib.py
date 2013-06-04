bdabee602d3c2661799e72cfa2cfc906c16c6faa
Vishalsai Daswani <vishalsai@gmail.com>
2013-06-03 23:38:33 -0700

    deleted old

--- SFTPLib.py
+++ SFTPLib.py
@@ -8,7 +8,7 @@ class SFTPServer():
 		self.transport = paramiko.Transport((hostname, port))
 		self.transport.connect(username=username, password=password)
 		self.sftp = paramiko.SFTPClient.from_transport(self.transport)
-		self.homedirectory = self.sftp.getcwd()
+
 
 	def pwd(self):
 		return self.sftp.getcwd()
@@ -57,106 +57,3 @@ s = SFTPServer(username=uname, password=pwd, hostname=hname, port=port)
 # s.download("/Users/vishalsaidaswani/Desktop/github keyart.png", "/Users/vishalsaidaswani/a.png")
 print s.ls()
 s.close()
-
-
-
-
-
-
-
-# print "Username: %s\nPassword: %s" % (username, password)
-
-# def ls(args):
-# 	print "yolo"
-# 	print args
-# 	s = paramiko.SSHClient()
-# 	s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# 	s.load_system_host_keys()
-# 	s.connect(hostname, port, username, password)
-# 	command = "ls " + args[0]
-# 	(stdin, stdout, stderr) = s.exec_command(command)
-# 	for line in stdout.readlines():
-# 		print line
-# 	s.close()
-
-# def mkdir(args):
-# 	#args = [directory_name]
-# 	print "swag"
-# 	print args
-# 	s = paramiko.SSHClient()
-# 	s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# 	s.load_system_host_keys()
-# 	s.connect(hostname, port, username, password)
-# 	command = "mkdir " + args[0]
-# 	(stdin, stdout, stderr) = s.exec_command(command)
-# 	for line in stdout.readlines():
-# 		print line
-# 	s.close()
-
-# def pwd(args):
-# 	#args = []
-# 	print "pwd"
-# 	s = paramiko.SSHClient()
-# 	s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# 	s.load_system_host_keys()
-# 	s.connect(hostname, port, username, password)
-# 	command = "pwd"
-# 	(stdin, stdout, stderr) = s.exec_command(command)
-# 	for line in stdout.readlines():
-# 		print line
-# 	s.close()
-
-# def transferFile(args, direction):
-# 	if (direction != 'download' or direction != 'upload'):
-# 		raise Exception('Transfer file. Inapplication direction specified')
-# 	if (len(args) < 2):
-# 		raise Exception('Transfer file - requires at least 2 args')
-# 	source_file = args[0]
-# 	destination_file = args[1]
-
-# 	transport = paramiko.Transport((hostname, port))
-# 	transport.connect(username=username, password=password)
-# 	sftp = paramiko.SFTPClient.from_transport(transport)
-
-# 	if (direction == 'download'):
-# 		sftp.get(source_file, destination_file)
-# 	else: # upload
-# 		sftp.put(source_file, destination_file)
-# 	sftp.close()
-# 	transport.close()
-
-
-# def getFileList(args):
-# 	# args = [directory_path_name]
-# 	print getFileList
-# 	transport = paramiko.Transport((hostname, port))
-# 	transport.connect(username=username, password=password)
-# 	sftp = paramiko.SFTPClient.from_transport(transport)
-# 	if (len(args) == 0):
-# 		print sftp.listdir()
-# 	else:
-# 		print sftp.listdir(args[0])
-# 	sftp.close()
-# 	transport.close()
-
-
-# d = {"ls" : ls, "mkdir" : mkdir, "upload" : uploadFile, "download" : downloadFile, "pwd" : pwd}
-
-# while True:
-# 	x = raw_input("Command: ")
-# 	print x
-# 	x = x.split()
-# 	args = []
-
-# 	if (len(x) == 0):
-# 		continue
-# 	elif (len(x) > 1):
-# 		args = x[1::]
-
-# 	fnMap = d.get(x[0])
-# 	if fnMap:
-# 		fnMap(args)
-# 	else:
-# 		print "no such function mapping. try again"
-# 		continue
-
